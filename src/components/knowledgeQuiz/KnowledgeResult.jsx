2d72b4ea316596da5d6dfa630af0a838c042e65c
RecheEduardo <Eduardorechemartins2006@gmail.com>
2024-11-20 16:02:20 -0300

    link-path-fix

--- src/components/knowledgeQuiz/KnowledgeResult.jsx
+++ src/components/knowledgeQuiz/KnowledgeResult.jsx
@@ -7,7 +7,7 @@ const KnowledgeResult = () => {
     <div className='result-quiz popIn'>
       <h1>Seu aproveitamento final é: <span>{parseFloat((userScore / 3) * 100).toFixed(1)}</span> %</h1>
       <p>Você acertou <strong>{userScore}</strong> de 3 questões</p>
-      <a href="../chooseQuiz/Choose.jsx"><button className='back-btn'>Voltar</button></a>
+      <a href="./"><button className='back-btn'>Voltar</button></a>
     </div>
   )
 }
